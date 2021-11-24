18badc5f73e4871f05f26bf7dcf7283742ab2f21
Siverbeast2003 <94976377+Siverbeast2003@users.noreply.github.com>
2021-11-24 21:16:31 +0700

    24/11/2021
    sex

--- baldski.py
+++ baldski.py
@@ -6,31 +6,35 @@ from ursina.shaders import lit_with_shadows_shader
 #load window
 app = Ursina()
 
-#main program
+#Entity
 map = Entity(model = 'maze.blend'
-,texture = "grass_texture"
-, scale = 20
+,texture = "Barski_text"
+, scale = 2
 , collider = 'mesh'
 , shader = lit_with_shadows_shader) #Set model
 
 amogus = Entity(model = "amogus.blend"
-,texture = "A_Texture", scale = 20,collider = 'mesh', shader = lit_with_shadows_shader
-, rotation =(0,0,0))
+,texture = "Barski_text", scale = 1,collider = 'mesh', shader = lit_with_shadows_shader
+, rotation =(12,12,12))
 base = Entity(model = "base.blend"
 , texture = "water.png", scale = 20,collider = 'mesh', shader = lit_with_shadows_shader)
+baldski = Entity(model = "baldski.blend"
+, texture = "Barski_text.png", scale = 30,collider = 'mesh', shader = lit_with_shadows_shader)
+baldski.position=Vec3(0,0,0)
 #Environment
-sky_texture = load_texture('assets/skybox.png')
+sky_texture = load_texture('Barski_text')
 sky = Sky()
 #movement
-player = FirstPersonController(model = 'player')
+player = FirstPersonController(model = 'player',speed=30)
 
-def update(): #Fix freefall
-    if player.y < -1:
-        player.position = Vec3(0,0,0)
+# def update(): #Fix freefall
+#     if player.y < -1:
+#         player.position = Vec3(0,0,0)
+player.position=Vec3(0,3,0)
 
 #Rotation
 def rotation():
-    amogus.rotation_y += 50 * time.dt
+    amogus.rotation_y += 999 * time.dt
     amogus.position += amogus.forward * time.dt
 
 #run window
