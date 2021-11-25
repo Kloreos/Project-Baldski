b7f9f7641676036c3d5fd8196f22f26f4019f706
Peter Dinh <54533061+PeterBenj2000@users.noreply.github.com>
2021-11-25 16:30:50 +0700

    Added barski gun

--- baldski.py
+++ baldski.py
@@ -1,8 +1,12 @@
-#Test Message
 #Import  library
 from ursina import *
+from ursina import collider
 from ursina.prefabs.first_person_controller import *
+from ursina.prefabs.first_person_controller import camera
 from ursina.shaders import lit_with_shadows_shader
+from ursina import curve
+
+import time
 #load window
 app = Ursina()
 
@@ -13,29 +17,61 @@ map = Entity(model = 'maze.blend'
 , collider = 'mesh'
 , shader = lit_with_shadows_shader) #Set model
 
+#Walls
+wall = Entity(model = "cube"
+, position = Vec3(1.39086, 0.752942, 23.2763)
+, scale = (80,70,1)
+, collider = "mesh"
+, texture = "Barski_text")
+wall2 = Entity(model = "cube"
+, position = Vec3(22.7444, 0.752942, 3.30865)
+, scale = (80,70,1)
+, collider = "mesh"
+, rotation_y = 90
+, texture = "Barski_text")
+wall3 = Entity(model = "cube"
+, position = Vec3(-1.09923, 0, -21.8463)
+, scale = (80,70,1)
+, collider = "mesh"
+, texture = "Barski_text")
+wall4 = Entity(model = "cube"
+, position = Vec3(-22.9727, 0, 3.39197)
+, scale = (80,70,1)
+, collider = "mesh"
+, rotation_y = 90
+, texture = "Barski_text")
+# other entities
 amogus = Entity(model = "amogus.blend"
-,texture = "Barski_text", scale = 1,collider = 'mesh', shader = lit_with_shadows_shader
-, rotation =(12,12,12))
+,texture = "Barski_text", scale = 10,collider = 'mesh', shader = lit_with_shadows_shader)
+
+amogus.position = Vec3(2,6,0)
+
 base = Entity(model = "base.blend"
 , texture = "water.png", scale = 20,collider = 'mesh', shader = lit_with_shadows_shader)
 baldski = Entity(model = "baldski.blend"
-, texture = "Barski_text.png", scale = 30,collider = 'mesh', shader = lit_with_shadows_shader)
-baldski.position=Vec3(0,0,0)
+, texture = "Barski_text.png", scale = 1, shader = lit_with_shadows_shader)
+baldski.position = (2,5,0)
+
+#gun 
+gun = Entity(parent = camera,model = "source/Gun low poly.fbx", scale = 0.25
+,position = Vec3(2,-0.25,2.5), rotation = Vec3(0,90,0), texture = "textures/Gun_low_poly_gun_AlbedoTransparency.png")
+
+# gun = Entity(model="source/Gun low poly.fbx"
+# ,  texture = "Barski_text.png", scale = 0.5, shader = lit_with_shadows_shader)
+# position = Vec3(2,5,0)
+
 #Environment
 sky_texture = load_texture('Barski_text')
 sky = Sky()
 #movement
 player = FirstPersonController(model = 'player',speed=30)
-
+baldski_pos = player.position + (3,3,0) 
 def update(): #Fix freefall
-    if player.y < -1:
-        player.position = Vec3(0,0,0)
-player.position=Vec3(0,3,0)
+    print(player.position)
+    # if player.y < -1: 
+    # player.position = Vec3(0,3,0)
 
-#Rotation
-def rotation():
-    amogus.rotation_y += 999 * time.dt
-    amogus.position += amogus.forward * time.dt
+
 
 #run window
 app.run()
