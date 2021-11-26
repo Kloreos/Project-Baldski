ec77e58b7aaf316fe141e24976076de9a85a6e15
Peter Dinh <54533061+PeterBenj2000@users.noreply.github.com>
2021-11-26 11:05:23 +0700

    Re-Added Amogus to follow

--- baldski.py
+++ baldski.py
@@ -11,6 +11,7 @@ import time
 app = Ursina()
 #Baldski texture
 BTexture = "Barski_text"
+WallB = "WallTexture.jpg"
 #Entity
 map = Entity(model = 'maze.blend'
 ,texture = BTexture
@@ -23,36 +24,47 @@ wall = Entity(model = "cube"
 , position = Vec3(1.39086, 0.752942, 23.2763)
 , scale = (80,70,1)
 , collider = "mesh"
-, texture = BTexture)
+, texture = WallB)
 wall2 = Entity(model = "cube"
 , position = Vec3(22.7444, 0.752942, 3.30865)
 , scale = (80,70,1)
 , collider = "mesh"
 , rotation_y = 90
-, texture = BTexture)
+, texture = WallB)
 wall3 = Entity(model = "cube"
 , position = Vec3(-1.09923, 0, -21.8463)
 , scale = (80,70,1)
 , collider = "mesh"
-, texture = BTexture)
+, texture = WallB)
 wall4 = Entity(model = "cube"
 , position = Vec3(-22.9727, 0, 3.39197)
 , scale = (80,70,1)
 , collider = "mesh"
 , rotation_y = 90
-, texture = BTexture)
+, texture = WallB)
 wall5 = Entity(model = "cube"
 , position = Vec3(2.38985, 30, -0.799802)
 , scale = (80,70,1)
 , collider = "mesh"
 , rotation_x =90     
-, texture = BTexture)
+, texture =WallB)
+
+#player
+player = FirstPersonController(model = 'player',speed=30)
+player.scale = 2
+baldski_pos = player.position + (3,3,0) 
+
 # other entities
 amogus = Entity(model = "amogus.blend"
-,texture =BTexture, scale = 10,collider = 'mesh', shader = lit_with_shadows_shader)
+,texture =BTexture, scale = 4,collider = 'mesh', shader = lit_with_shadows_shader)
 
-amogus.position = Vec3(2,6,0)
+amogus.position = Vec3(2,5,0)
+amogus.add_script(SmoothFollow(target = player, offset =[0,5,0], speed = 4))
 
+def attack():
+    pass
+
+#Base
 base = Entity(model = "base.blend"
 , texture = "water.png", scale = 20,collider = 'mesh', shader = lit_with_shadows_shader)
 baldski = Entity(model = "baldski.blend"
@@ -70,16 +82,9 @@ weapon2 = Entity(model = basic_gun, scale = 0.05
 
 #Weapon animations
 
-
 #Environment
 sky = Sky()
-#movement
-player = FirstPersonController(model = 'player',speed=30)
-player.scale = 2
-baldski_pos = player.position + (3,3,0) 
 
-run_speed = 50
-run = False
 
 def update(): #upate function
     # print(player.position)
@@ -111,9 +116,6 @@ def update(): #upate function
         weapon.position =Vec3(2,-2,-0.5)
         weapon.rotation =Vec3(2,0,8)
         weapon.scale =  0.05
-
-
-
 
 #run window
 app.run()
